f36903d3074332fbb4b02df797344088bbde75cd
jasole1983 <66217743+jasole1983@users.noreply.github.com>
2021-06-20 22:03:10 -0700

    Update index.js

--- frontend/src/components/LandingPage/index.js
+++ frontend/src/components/LandingPage/index.js
@@ -1,23 +1,15 @@
-// import { useState, useEffect } from 'React'
-// import { dispatch } from 'react-redux'
+
 
 
 function Landing(){
-//     const [workInProgress, setWorkInProgress] = useState(false)
+
     let workInProgress = true;
-//     useEffect(() => {
-//         dispatch(setWorkInProgress(true))
-//     }, [workInProgress])
+
 
     const background = workInProgress ? <img src='./underconstruction.jpg' /> : <img src='./custombackgroun2d.jpg' class="bkgrd-img" />
 
     return (
-        <div class="bkgrd-img-container">
-                {background}
-//                 {setTimeout( () => {
-//                     return setWorkInProgress(true)
-//                 }, 5000)}
-        </div>
+        <div class="bkgrd-img-container">{background}</div>
     )
 }
 
