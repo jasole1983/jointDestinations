3ae44501696e50daa70cb912d975ba79bb782de6
Jason Leatiota <jasole2007@yahoo.com>
2021-06-24 09:04:49 -0700

    missed 2

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -1,52 +1,56 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-import ProfileButton from './ProfileButton';
-import LoginFormModal from '../LoginFormModal/index';
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { useSelector } from "react-redux";
+import ProfileButton from "./ProfileButton";
+import LoginFormModal from "../LoginFormModal/index";
 
-function Navigation({ isLoaded }){
-  const sessionUser = useSelector(state => state.session.user);
+function Navigation({ isLoaded }) {
+	const sessionUser = useSelector((state) => state.session.user);
 
-  let sessionLinks;
-  if (sessionUser) {
-    sessionLinks = (
-      <>
-        <ProfileButton user={sessionUser} />
-      </>  
-    );
-  } else {
-    sessionLinks = (
-      <>      
-        <div id="home-container" className="navi">
-          <LoginFormModal />
-          <button className="navi_btn"> 
-            <NavLink exact to="/" className="navi_btn"><i className="fas fa-cannabis"/></NavLink>
-          </button>
-        </div>
-      </>
-    );
-  }
-
-
-  return (
-    <div className="nav_bar">
-      <div className="app">UNLIT</div>
-      <div className="ints">
-        <div id="checkin-div" className="ints_link">
-          <NavLink to="/checkin" className="dirNavLink">Check-In</NavLink>
-        </div>
-        <div id="nearby-div" className="ints_link">
-          <NavLink to="/nearby" className="dirNavLink">Events Near Me</NavLink>
-        </div>
-        <div id="review-div" className="ints_link">
-          <NavLink to="/reviews" className="dirNavLink">Highest Rated</NavLink>
-        </div>
-      </div>
-      <div className="navi">
-          {isLoaded && sessionLinks}
-      </div>
-    </div>
-  );
+	let sessionLinks;
+	if (sessionUser) {
+		sessionLinks = (
+			<>
+				<ProfileButton user={sessionUser} />
+			</>
+		);
+	} else {
+		sessionLinks = (
+			<>
+				<div id='home-container' className='navi'>
+					<LoginFormModal />
+					<button className='navi_btn'>
+						<NavLink exact to='/' className='navi_btn'>
+							<i className='fas fa-cannabis' />
+						</NavLink>
+					</button>
+				</div>
+			</>
+		);
+	}
+	return (
+		<div className='nav_bar'>
+			<div className='app'>UNLIT</div>
+			<div className='ints'>
+				<div id='checkin-div' className='ints_link'>
+					<NavLink to='/checkin' className='dirNavLink'>
+						Check-In
+					</NavLink>
+				</div>
+				<div id='nearby-div' className='ints_link'>
+					<NavLink to='/nearby' className='dirNavLink'>
+						Events Near Me
+					</NavLink>
+				</div>
+				<div id='review-div' className='ints_link'>
+					<NavLink to='/reviews' className='dirNavLink'>
+						Highest Rated
+					</NavLink>
+				</div>
+			</div>
+			<div className='navi'>{isLoaded && sessionLinks}</div>
+		</div>
+	);
 }
 
 export default Navigation;
