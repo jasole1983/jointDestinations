7863c9e9903576c77b2cd37616fd60c2bb34bcb4
Jason Leatiota <jasole2007@yahoo.com>
2021-06-24 17:04:39 -0700

    class over

--- frontend/src/store/users.js
+++ frontend/src/store/users.js
@@ -1,11 +1,9 @@
 
 
-
-const USER_CHECKIN = "users/USER_CHECKIN";
-const USER_ADD_FAVORITE = "users/USER_ADD_FAVORITE";
-const USER_REMOVE_FAVORITE = "users/USER_REMOVE_FAVORITE";
-const LOAD_USER_FAVORITES = "users/LOAD_USER_FAVORITES";
-const LOAD_USER_HISTORY = "users/LOAD_USER_HISTORY";
+const LOAD_PROFILE = "/users/LOAD_PROFILE"
+const USER_CHECKIN = "/users/USER_CHECKIN";
+const UPDATE_PROFILE = "/users/UPDATE_PROFILE;"
+const LOAD_USER_HISTORY = "/users/LOAD_USER_HISTORY";
 
 const checkIn = (location, user) => ({
   type: USER_CHECKIN,
@@ -13,45 +11,64 @@ const checkIn = (location, user) => ({
   user,
 });
 
-const addFav = (targetId, user) => ({
-  type: USER_ADD_FAVORITE,
-  targetId,
-  user,
-});
-
-const remFav = (targetId, user) => ({
-  type: USER_REMOVE_FAVORITE,
-  targetId,
-  user,
-});
-
-const loadFav = (list, user) => ({
-  type: LOAD_USER_FAVORITES,
-  list,
-  user,
-});
-
-const loadHist = (obj, user) => ({
+const loadHist = (user) => ({
   type: LOAD_USER_HISTORY,
-  obj,
   user,
 })
 
-export const getUserFavs = (id) => async dispatch => {
-  const res = await fetch(`/api/users/${id}/favorites`)
+const getProfile = (userId) => ({
+  type: LOAD_PROFILE,
+  userId,
+})
 
+const updateProfile = (userId) => ({
+  type: UPDATE_PROFILE,
+  userId,
+})
+
+export const getUserHist = (id) => async dispatch => {
+  const res = await fetch(`/api/users/${id}/history`)
+  
   if(res.ok){
-    const favorites = await res.json()
-    dispatch(loadFav(favorites, id))
-    return favorites;
+    const history = await res.json()
+    dispatch(loadHist(history, id))
+    return history;
   }
 }
 
-export const userCheckin = (location, id ) => async dispatch => {
+
+export const getUserProfile = (id) => async dispatch => {
+  const res = await fetch(`/api/users/${id}`)
+  
+  if(res.ok){
+    const profile = await res.json()
+    dispatch(getProfile(profile))
+    return profile;
+  }
+}
+
+
+export const updateProfile = (user) => async dispatch => {
+  const res = await fetch(`/api/users/${user.id}`, {
+    method: 'PUT',
+    headers: {
+    'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(user)
+  })
+  
+  if(res.ok){
+    const updated = await res.json()
+    dispatch(updateProfile(history, id))
+    return history;
+  }
+}
+
+export const userCheckIn = (location, id ) => async dispatch => {
   const data = {location, id, time: Date().now};
-  const res = await fetch(`/api/users/${id}`, 
+  const res = await fetch(`/api/location/${id}/${id}`, 
       {
-      method: 'PUT',
+      method: 'POST',
       headers: {
         'Content-Type':'application/json'
       },
@@ -64,15 +81,7 @@ export const userCheckin = (location, id ) => async dispatch => {
     return checkedInUser;
   }
 };
-export const getUserHist = (id) => async dispatch => {
-  const res = await fetch(`/api/users/${id}/history`)
-  
-  if(res.ok){
-    const history = await res.json()
-    dispatch(loadHist(history, id))
-    return history;
-  }
-}
+
 
 
 
@@ -88,13 +97,17 @@ const userReducer = (state = initialState, action) => {
          [action.user.id]: action.user,
     };
   };
-    // case USER_ADD_FAVORITE:
-    //   return state + action.payload;
-    // case USER_REMOVE_FAVORITE:
-    //   return state - action.payload;
-    // case LOAD_USER_FAVORITES:
-    //   return state 
-    case LOAD_USER_HISTORY:
+    case LOAD_USER_HISTORY: {
+      const newState = {
+        ...state,
+        [action.user.id]: action.user.createdAt;
+      };
+      const history = newState.list.map(id => newState[id]);
+      history.push(action.user);
+      newState.list = sortList(history);
+      return newState;
+    }
+    case 
 
 
     default:
