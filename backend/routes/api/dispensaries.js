a96a95bf89ee62706726161e7172dd0c21a6797c
Jason Leatiota <jasole2007@yahoo.com>
2021-06-28 10:55:44 -0700

    routes

---
+++ backend/routes/api/dispensaries.js
@@ -0,0 +1,58 @@
+const express = require('express')
+const router = express.Router();
+const asyncHandler = requrie('express-async-handler');
+const { setTokenCookie, requreAuth } = requre('../index.js')
+const db = requre('../../db/models/')
+const { Users, Dispensaries, Flowers, Reviews } = db
+
+
+router.post('/:id/review', asyncHandler( async (req, res) => {
+  const dispensaryId = parseInt(req.params.id, 10);
+  const currentUserId = res.locals.user.id;
+  const { rating, comments, photoURL} = req.body.data
+  const newReview = await Reviews.build({
+    targetType: 'Dispensary',
+    dispensaryId,
+    rating,
+    comments,
+    userId: currentUserId,
+    photoURL})
+    // tbd validations
+    await newReview.save() ;
+    
+    res.status(203);
+  }))
+  router.post('/:id/flower', asyncHandler( async (req, res) => {
+    const dispensaryId = parseInt(req.params.id, 10);
+    const currentUserId = res.locals.user.id
+    const { commonName, botanicalName, imgURL, THC, strain, reportedEffects } = req.body.data
+    const newFlower = await Flowers.build({
+      commonName,
+      botanicalName,
+      imgURL,
+      THC,
+      strain,
+      reportedEffects})
+      // tbd validations
+      await newFlower.save() ;
+      
+      res.status(203);
+    }))
+    
+    
+    router.get('/:id', asyncHandler( async (req, res) => {
+      const dispensaryId = parseInt(req.params.id, 10);
+      
+      const dispensaryData = await Dispensaries.findAll({ include: [Flowers, Reviews], where: { dispensaryId }})
+      
+      await console.log('dispensary', dispensary)
+      
+      res.json({ dispensaryData })
+    }))
+    
+    router.get('/', asyncHandler( async (req, res) => {
+      const listOfDisp = [];
+    
+    }))
+    
+    module.exports = router
